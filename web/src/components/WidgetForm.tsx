b5a30a666fef4fa6a08d1a603fe1392a1c8639e3
Wanderson Santos <wanderson.santos@gcbinvestimentos.com>
2022-05-03 10:31:14 -0300

    feat: create close button

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,7 +1,25 @@
-export function WidgetForm(){
-    return(
-        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-            Hello World
-        </div>
-    )
+import { CloseButton } from "./CloseButton";
+
+export function WidgetForm() {
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      <header>
+        <span className="text-xl leading-6">Deixe seu feedback</span>
+
+        <CloseButton />
+      </header>
+
+      <p>Hello world</p>
+
+      <footer className="text-xs text-neutral-400">
+        Feito por{" "}
+        <a
+          className="underline underline-offset-2"
+          href="https://www.linkedin.com/in/wandersongsantos/"
+        >
+          Wanderson Santos
+        </a>
+      </footer>
+    </div>
+  );
 }
