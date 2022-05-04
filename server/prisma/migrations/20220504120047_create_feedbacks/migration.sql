b0d45b960b925c3f1b9bddff0c9e1a901fbd1d4c
Wanderson Santos <wanderson.santos@gcbinvestimentos.com>
2022-05-04 09:06:18 -0300

    feat: create model feedbacks

---
+++ server/prisma/migrations/20220504120047_create_feedbacks/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE `feedbacks` (
+    `id` VARCHAR(191) NOT NULL,
+    `type` VARCHAR(191) NOT NULL,
+    `comment` VARCHAR(191) NOT NULL,
+    `screenshot` VARCHAR(191) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
