2e2ecb3ad223501d7dbb2a0245f7febc456636a0
Wanderson Santos <wanderson.santos@gcbinvestimentos.com>
2022-05-04 09:24:24 -0300

    feat: create feedback route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,25 @@
-import express from 'express';
+import express from "express";
+import { prisma } from "./prisma";
 
 const app = express();
+app.use(express.json());
 
 const port = 5000;
 
+app.use("/feedbacks", async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  });
+
+  res.status(201).json(feedback)
+});
+
 app.listen(port, () => {
-    console.log(`Server is running in http://localhost:${port}`)
+  console.log(`Server is running in http://localhost:${port}`);
 });
