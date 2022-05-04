80be5d3921c6f90700266752af107497048336e9
Wanderson Santos <wanderson.santos@gcbinvestimentos.com>
2022-05-04 00:38:48 -0300

    feat: success message after sending feedback

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -5,7 +5,7 @@ import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
 import bugImageUrl from "../../assets/bug.svg";
 import ideaImageUrl from "../../assets/idea.svg";
 import otherImageUrl from "../../assets/thought.svg";
-
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessSteps";
 
 export const feedbackTypes = {
   BUG: {
@@ -35,31 +35,41 @@ export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
 
   function handleRestarFeedback() {
+    setFeedbackSent(false);
     setFeedbackType(null);
   }
 
   return (
-    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 
-    flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      {!feedbackType ? (
-        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      {feedbackSent ? (
+        <FeedbackSuccessStep onFeedbackRestartRequested={handleRestarFeedback} />
       ) : (
-        <FeedbackContentStep
-          feedbackType={feedbackType}
-          onFeedbackRestartRequested={handleRestarFeedback}
-        />
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+          ) : (
+            <FeedbackContentStep
+              feedbackType={feedbackType}
+              onFeedbackRestartRequested={handleRestarFeedback}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
       )}
 
       <footer className="text-xs text-neutral-400">
-        Feito por
-        <a
-          className="underline underline-offset-2"
-          href="https://www.linkedin.com/in/wandersongsantos/"
-        >
-          Wanderson Santos
-        </a>
+        <p>
+          Feito por
+          <a
+            className="underline underline-offset-2"
+            href="https://www.linkedin.com/in/wandersongsantos/"
+          >
+            Wanderson Santos
+          </a>
+        </p>
       </footer>
     </div>
   );
