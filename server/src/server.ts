a27410051af7aa8cce994222dd9b88ebc3e6f90b
Wanderson Santos <wanderson.santos@gcbinvestimentos.com>
2022-05-07 03:07:44 -0300

    fix: port server deploy

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,7 +4,7 @@ import { routes } from "./routes";
 
 const app = express();
 
-const port = 5000 || process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
